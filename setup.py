d3a08e69e352903c589f35a401936f488da337f5
Rich Wareham <rjw57@cam.ac.uk>
2014-12-02 14:57:15 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='burisim',
+    version='0.1.0',
+    description='Simulator for buri machine',
+    author='Rich Wareham',
+    author_email='rich.buri@richwareham.com',
+    packages=find_packages(),
+    install_requires='py65',
+    entry_points={
+        'console_scripts': [
+            'burisim = burisim:main',
+        ],
+    },
+)
