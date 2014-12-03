5d585eefb938062ac2575d7ab74c96f0f7fca54a
Rich Wareham <rjw57@cam.ac.uk>
2014-12-03 11:46:53 +0000

    re-factor simulator into separate class

--- burisim/__init__.py
+++ burisim/__init__.py
@@ -8,47 +8,19 @@ Usage:
 Options:
     -h, --help          Show a brief usage summary.
 """
-from __future__ import print_function
-from future.builtins import * # pylint: disable=wildcard-import
+from __future__ import absolute_import, division, print_function
+from builtins import * # pylint: disable=wildcard-import,redefined-builtin
+from past.builtins import basestring # pylint: disable=redefined-builtin
 
 from itertools import chain
+import logging
 import struct
 
 from py65.devices.mpu6502 import MPU
 from py65.memory import ObservableMemory
 from docopt import docopt
 
-def main():
-    opts = docopt(__doc__)
-
-    # Read ROM
-    rom_bytes = open(opts['<rom>'], 'rb').read()
-    print('Read {} bytes of ROM'.format(len(rom_bytes)))
-
-    # Create memory space
-    mem = ObservableMemory(addrWidth=MPU.ADDR_WIDTH)
-
-    # Create microprocessor
-    mpu = MPU(memory=mem)
-
-    # Copy ROM from 0xC000 to 0xFFFF. Loop if necessary.
-    for addr, val in zip(range(0xC000, 0x10000), chain(rom_bytes)):
-        mem[addr] = struct.unpack('B', val)[0]
-
-    # Register all observers on this memory
-    setup_memory_map(mem)
-
-    # Reset CPU reading reset vector
-    mpu.pc = mpu.WordAt(0xFFFC)
-    while True:
-        mpu.step()
-
-def setup_memory_map(mem):
-    """Registers observers on ObservableMemory mem corresponding to the buri
-    memory map.
-
-    """
-    set_ro_mem_(mem, range(0xC000, 0xFFFF))
+_LOGGER = logging.getLogger(__name__)
 
 class ReadOnlyMemoryError(RuntimeError):
     """Raised when code had attempter to write to read-only memory."""
@@ -56,17 +28,82 @@ class ReadOnlyMemoryError(RuntimeError):
         self.address = address
         self.value = value
         super(ReadOnlyMemoryError, self).__init__(
-            'Illegal attempt to write {.address} to {.value}'.format(self)
+            'Illegal attempt to write ${0.value:02X} to ${0.address:04X}'.format(self)
         )
 
-def set_ro_mem_(mem, rng):
-    """Mark the specified range of memory as read-only. Raise a
-    ReadOnlyMemoryError if read from.
+class BuriSim(object):
+    ROM_RANGE = 0xC000, 0x10000
 
-    """
-    def raise_hell(address, value):
-        raise ReadOnlyMemoryError(address, value)
-    mem.subscribe_to_write(rng, raise_hell)
+    def __init__(self):
+        # Create memory space
+        self.mem = ObservableMemory(addrWidth=MPU.ADDR_WIDTH)
+
+        # Create microprocessor
+        self.mpu = MPU(memory=self.mem)
+
+        # Setup memory observers
+        self._raise_on_rom_write = True
+        self._add_mem_observers()
+
+        # Reset the computer
+        self.reset()
+
+    def load_rom(self, fobj_or_string):
+        """Load a ROM image from the passed file object or filename-string. The
+        ROM is truncated or repeated as necessary to fill the buri ROM region.
+
+        """
+        if isinstance(fobj_or_string, basestring):
+            with open(fobj_or_string, 'rb') as fobj:
+                self._load_rom_from_bytes(fobj.read())
+        else:
+            self._load_rom_from_bytes(fobj_or_string.read())
+
+    def reset(self):
+        """Perform a hardware reset."""
+        # Reset MPU
+        self.mpu.reset()
+
+        # Read reset-vector
+        self.mpu.pc = self.mpu.WordAt(MPU.ResetTo)
+
+    def step(self):
+        """Single-step the processor."""
+        self.mpu.step()
+
+    def _load_rom_from_bytes(self, rom_bytes):
+        """Internal method to actually load ROM image from bytes."""
+        _LOGGER.info(
+            'Loading %s bytes from ROM image of %s bytes',
+            BuriSim.ROM_RANGE[1] - BuriSim.ROM_RANGE[0], len(rom_bytes)
+        )
+
+        # Copy ROM from 0xC000 to 0xFFFF. Loop if necessary.
+        self._raise_on_rom_write = False
+        for addr, val in zip(range(*BuriSim.ROM_RANGE), chain(rom_bytes)):
+            self.mem[addr] = struct.unpack('B', val)[0]
+        self._raise_on_rom_write = True
+
+    def _add_mem_observers(self):
+        """Internal method to add read/write observers to memory."""
+        def raise_hell(address, value):
+            if self._raise_on_rom_write:
+                raise ReadOnlyMemoryError(address, value)
+        self.mem.subscribe_to_write(range(*BuriSim.ROM_RANGE), raise_hell)
+
+def main():
+    opts = docopt(__doc__)
+
+    # Create simulator
+    sim = BuriSim()
+
+    # Read ROM
+    sim.load_rom(opts['<rom>'])
+
+    # Step
+    sim.reset()
+    while True:
+        sim.step()
 
 if __name__ == '__main__':
     main()
