664b63d608da808bccda32993c2f3c92458c77c2
Rich Wareham <rjw57@cam.ac.uk>
2015-03-01 00:23:02 +0000

    init.s: add copyright banner

--- src/init.s
+++ src/init.s
@@ -27,10 +27,12 @@
 	jsr initscr		; reset terminal
 
 	jsr cls			; clear screen
-	ldw ptr1, banner_str	; write banner
+	ldw ptr1, banner1_str	; write banner
+	jsr putln
+	ldw ptr1, banner2_str
 	jsr putln
 
-@prompt_loop:
+prompt_loop:
 	lda #'*'		; write command prompt
 	jsr putc
 
@@ -44,25 +46,25 @@
 	jsr readln
 
 	lda line_buffer		; line zero-length? (i.e. first byte is nul)
-	beq @prompt_loop	; yes, loop back to prompt
+	beq prompt_loop	; yes, loop back to prompt
 
 	jsr splitcli		; split line
 
 	jsr findcmd		; find command
 	cmp #0			; found?
-	bne @found_command	; yes
-@no_command:
+	bne found_command	; yes
+no_command:
 	ldw ptr1, nsc_str	; no, print error
 	jsr putln
-	bra @prompt_loop	; branch
+	bra prompt_loop	; branch
 
-@found_command:
+found_command:
 	jsr run_command		; run it
-	bra @prompt_loop	; loop
+	bra prompt_loop	; loop
 
 ; Sit in a tight loop for the rest of time.
-@halt_loop:
-	bra @halt_loop
+halt_loop:
+	bra halt_loop
 .endproc
 
 ; INTERNAL proc. Jump to address stored in ptr1.
@@ -71,7 +73,9 @@
 .endproc
 
 .segment "RODATA"
-banner_str:
+banner1_str:
 	cstring "Buri microcomputer"
+banner2_str:
+	cstring "(C) 2015 Rich Wareham <rich.buri@richwareham.com>"
 nsc_str:
 	cstring "No such command"
