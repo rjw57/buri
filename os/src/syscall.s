7d7da8fa442eef5a77f15e5e9cedcda502ca2cd5
Rich Wareham <rjw57@cam.ac.uk>
2015-04-21 23:57:33 +0100

    syscall: increase efficiency of jump table
    make use of the additional 65C02 addressing mode to make the syscall table more
    efficient

--- os/src/syscall.s
+++ os/src/syscall.s
@@ -1,14 +1,14 @@
 ; Single byte query/set routine
 
 ; syscall is a general purpose entry point to the OS which may optionally
-; transfer a byte as an argument. On entry, X selects which routine is used
-; and A contains the argument. On exit, A contains any return value and
-; processor flags may have been modified.
+; transfer a byte as an argument. On entry, A selects which routine is used
+; and X contains the argument. On exit, A contains any return value and
+; processor flags may have been modified and X is corrupted.
 ;
-; X	Routine
+; A	Routine
 ; 0	Query input buffer. Carry is set if input buffer non-empty.
 ;
-; Values of X other than those above will result in an error.
+; Values of A other than those above will result in an error.
 
 .include "globals.inc"
 .include "macros.inc"
@@ -16,41 +16,19 @@
 .export syscall
 
 .proc syscall
-	save_xy
-	save_word ptr1
-	save_word ptr2
+	; TODO: check value of A < 128
 
-	; TODO: check value of X
-
-	; Get the address of the syscall routine by adding X to
-	; jump_table twice
-	ldw ptr1, jump_table
-	pha
-	txa
-	add_word ptr1
-	add_word ptr1
+	; Set A, X = X, A*2. This allows the syscall routines to be called with
+	; A as the input argument.
+	asl
+	phx
+	tax
 	pla
 
-	; Then copying word at that address into ptr2
-	ldy #0
-	lda (ptr1), Y
-	sta ptr2, Y
-	iny
-	lda (ptr1), Y
-	sta ptr2, Y
-
-	; Jump to syscall routine at ptr2. Use JSR here to allow
-	; syscall's rts instruction to come back here.
-	jsr syscall_jump
-
-	restore_word ptr2
-	restore_word ptr1
-	restore_xy
-	rts
+	; Jump to syscall routine. Syscall routine's rts instruction will return
+	; to caller.
+	jmp (jump_table, X)
 .endproc
-
-syscall_jump:
-	jmp (ptr2)
 
 jump_table:
 	.import haveinput
