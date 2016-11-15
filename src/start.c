dbd79d6e5b086e962ebe6aea6e5f49b2c041037f
Rich Wareham <rich@richwareham.com>
2016-11-15 08:33:15 +0000

    remove unused console API usage from start.c

--- src/start.c
+++ src/start.c
@@ -12,18 +12,5 @@ static void puts(const char* s) {
 void start(void) {
     vdp_init();
     console_init();
-
-    console_cursor_set(3, 5);
-    console_write_char('X');
-    console_cursor_set(0, 0);
-    console_write_char('O');
-    console_cursor_set(10, 2);
-    console_write_char('/');
-    console_cursor_set(39, 2);
-    console_write_char('>');
-    console_cursor_set(38, 2);
-    console_write_char('A');
-    console_cursor_set(0, 0);
-
     while(1) { puts(msg); puts(" "); }
 }
