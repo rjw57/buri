00bf67ce75b5c7f85f929eeef17ce859cf29ae1f
Rich Wareham <rich@richwareham.com>
2016-11-21 22:56:22 +0000

    working recv command

--- src/cmd/recv.c
+++ src/cmd/recv.c
@@ -16,6 +16,9 @@
 
 #include "util.h"
 
+#define STX 0x02
+#define ACK 0x06
+
 static u8 read(void) {
     i16 v = -1;
     while(v < 0) { v = acia6551_recv_byte(); }
@@ -28,26 +31,26 @@ static void write(u8 v) {
 
 void recv(void) {
     u8 *ptr;
-    u8 sum1 = 0, sum2 = 0;
+    u8 sum1 = 0, sum2 = 0, v;
     u16 dest = parse_hex_16(cli_buf + cli_arg_offsets[0]);
-    u16 len = 0;
+    u16 len = 0, n;
 
     // wait for STX
-    while((len = read()) != 0x02) { }
-    putln("starting...");
-
+    while((len = read()) != STX) { }
     len = read();
     len |= ((u16)read()) << 8;
-    puts("length: ");
-    put_hex_16(len);
-    putln("");
 
-    for(ptr=(u8*)dest; len>0; --len, ++ptr) {
-        u8 v = read();
+    // read blocks
+    for(n=0; n<len; ++n) {
+        if((n & 0xFF) == 0) {
+            write(ACK); write(sum1); write(sum2);
+        }
+        v = read();
+        ((u8*)dest)[n] = v;
         sum1 += v;
         sum2 += sum1;
-        // *ptr = v;
     }
 
+    write(ACK);
     write(sum1); write(sum2);
 }
